7c29fc95addee1b4a6309029d77c3e26853163aa
bettykonjo <84689462+bettykonjo@users.noreply.github.com>
2022-05-31 16:54:28 -0400

    creating daycard components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import DayCard from './Components/DayCard';
 
 function App() {
   return (
     <div className="App">
        <h1>bh-weather-forecaster-project</h1>
+       < DayCard />
     </div>
   );
 }
---
+++ src/Components/DayCard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import weatherdatas from './WeatherBit-Sample-Data.json';
+
+function DayCard() {
+  return (
+    <div className="daycard">
+      <h1>seven day weather information</h1>
+      {weatherdatas && weatherdatas.map((weatherdata) => {
+          return (
+            <div className="weathers" key={weatherdata.id}>
+              <strong>{weatherdata.wind_cdir_full}</strong>
+              {weatherdata.weather && weatherdata.weather.map( data =>{
+                return(
+                  <div key={weatherdata.id}>
+                    {data.description}
+                  </div>
+                )
+              })}
+            </div>
+          );
+        })}
+    </div>
+  );
+}
+
+export default DayCard;
