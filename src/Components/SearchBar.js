b657f94ea73ea767202d0cf0506a2914864ed383
bettykonjo <84689462+bettykonjo@users.noreply.github.com>
2022-05-31 17:13:46 -0400

    adding a button

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -3,23 +3,26 @@ import React, { useState } from 'react';
 const SearchBar = () => {
   const [searchTerm, steSearchTerm] = useState('');
 
-  const handleClick = () =>{
+  const handleSubmit = () =>{
     steSearchTerm = ('')
   }
   return (
     <div>
         <h1>Weather Info for:{steSearchTerm}</h1>
-      <input
+        <form onSubmit={handleSubmit}>
+        <input
         className="search-text"
         type="text"
         value={searchTerm}
         placeholder="Type to search....."
         onChange={(e) => {
           steSearchTerm(e.target.value);
-          <button onClick={() => {handleClick('')}}> Search </button>
+          // <button onClick={() => {handleSubmit('')}}> Search </button>
         }}
       />
-      <button> Search </button>
+      <button onClick={() => {handleSubmit('')}}> Search </button>
+        </form>
+
     </div>
   );
 };
