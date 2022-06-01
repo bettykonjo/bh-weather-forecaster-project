67adb31700b07b8de36dae1b0b0e716c3254397a
bettykonjo <84689462+bettykonjo@users.noreply.github.com>
2022-06-01 11:17:25 -0400

    adding some functionality

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -3,26 +3,32 @@ import React, { useState } from 'react';
 const SearchBar = () => {
   const [searchTerm, steSearchTerm] = useState('');
 
-  const handleSubmit = () =>{
-    steSearchTerm = ('')
-  }
+  const handleSubmit = () => {
+    steSearchTerm = '';
+  };
   return (
     <div>
-        <h1>Weather Info for:{steSearchTerm}</h1>
-        <form onSubmit={handleSubmit}>
+      <h1>Weather Info for:{steSearchTerm}</h1>
+      <form onSubmit={handleSubmit}>
         <input
-        className="search-text"
-        type="text"
-        value={searchTerm}
-        placeholder="Type to search....."
-        onChange={(e) => {
-          steSearchTerm(e.target.value);
-          // <button onClick={() => {handleSubmit('')}}> Search </button>
-        }}
-      />
-      <button onClick={() => {handleSubmit('')}}> Search </button>
-        </form>
-
+          className="search-text"
+          type="text"
+          value={searchTerm}
+          placeholder="Type to search....."
+          onChange={(e) => {
+            steSearchTerm(e.target.value);
+            // <button onClick={() => {handleSubmit('')}}> Search </button>
+          }}
+        />
+        <button
+          onClick={() => {
+            handleSubmit('');
+          }}
+        >
+          {' '}
+          Search{' '}
+        </button>
+      </form>
     </div>
   );
 };
