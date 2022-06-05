3f4856fed49bacc747d71677a54620c91a3114ca
bettykonjo <84689462+bettykonjo@users.noreply.github.com>
2022-06-05 09:46:52 -0400

    creating footer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import DayCard from './Components/DayCard';
+import Footer from './Components/Footer';
 import SearchBar from './Components/SearchBar';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
        <  SearchBar />
        < DayCard />
+       <Footer/>
     </div>
   );
 }
