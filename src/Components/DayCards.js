eee6ad730041f69eb0a526da47c917f8d48952f6
bettykonjo <84689462+bettykonjo@users.noreply.github.com>
2022-06-16 17:24:45 -0400

    docs(daycards component):creating daycards component and its functionality

--- src/Components/DayCards.js
+++ src/Components/DayCards.js
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from 'react';
+import moment from 'moment';
+import styled from 'styled-components';
+import { useParams, useNavigate } from 'react-router-dom';
 
-function DayCards() {
+const DayCardsWrapper = styled.div`
+   .info{
+  background-color: wheat;
+  border-radius: 5px;
+  margin: 15px;
+  flex-wrap: wrap;
+  flex-direction: row;
+    }
+  h5 {
+    /* background-color: green; */
+  }
+
+`;
+const DayCards = ({ weekdays, daynum }) => {
+
+  const navigate = useNavigate();
+    // const { id:selectedDay } = useParams();
+  const { ts:selectedDay  } = useParams();
+ 
+  // const { ts } = useParams();
+  
+  const singledayinfo = (e) => {
+    navigate(`/DayDetails/${daynum}`);
+    // navigate(`/DayDetails/${ weekdays.ts}`);
+  };
+  
+
   return (
-    <div>DayCards</div>
-  )
-}
+    
+    
+    <DayCardsWrapper selectedDay={ selectedDay } onClick={singledayinfo}>    
+      {  
+       <div className='information'>
+        <h5>{moment(weekdays.valid_date).format('dddd')}</h5>
+        <div className="info"> 
+          <div className='temp'>
+            <strong>temp</strong>:{weekdays.temp}
+          </div>
+          <img
+            src={`https://www.weatherbit.io/static/img/icons/${weekdays.weather.icon}.png`}
+            alt="weather icon"
+          />
+          <div className='maxt'>
+            <strong>High</strong>:{weekdays.high_temp}
+          </div>
+          <div className='mint'>
+            <strong>Low</strong>:{weekdays.low_temp}
+          </div>
+          <div className='precip'>
+            <strong>precip</strong>:{weekdays.precip}%
+          </div>
+        </div>
+        </div>
+        
+      }
+    </DayCardsWrapper>  
+    
+  );
+};
 
-export default DayCards
+export default DayCards;
