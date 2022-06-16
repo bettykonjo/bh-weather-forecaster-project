9d74ba7cccfcd7027179caea4e7aa80346903c65
bettykonjo <84689462+bettykonjo@users.noreply.github.com>
2022-06-16 16:22:01 -0400

    finalize the search bar component

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,1 +1,57 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SearchBarWrapper = styled.div`
+  align-items: center;
+  color: blue;
+  align-content: flex-end;
+  text-align: flex-end;
+  h1 {
+    margin: 1rem 0;
+    text-align: start;
+  }
+  search-box {
+    border-radius: 1px solid black;
+  }
+  input {
+    font-size: 18px;
+    width: 60%;
+    height: 60%;
+    background-color: white;
+    color: black;
+    float: left;
+  }
+  .inputC:hover {
+    cursor: pointer;
+    color: blue;
+  }
+  button {
+    background-color: green;
+    color: white;
+    float: left;
+    padding-bottom: 6px;
+  }
+`;
+
+const SearchBar = ({ locations, setLocations, getGeoData }) => {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    getGeoData();
+  };
+  return (
+    <SearchBarWrapper>
+      <form onSubmit={onSubmit}>
+        <h1>Weather info for, {locations}:</h1>
+        <input
+          type="text"
+          placeholder="search for weather info"
+          value={locations}
+          onChange={(e) => setLocations(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+    </SearchBarWrapper>
+  );
+};
+export default SearchBar;
 
